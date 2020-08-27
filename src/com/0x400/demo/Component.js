0f539f65401a93ca1cfb22395bc7d1a1505975a7
Syncher Pylon, Peng <syncviip@gmail.com>
2020-08-27 17:28:48 +0800

    feat: step 10 - manifest.json

--- src/com/0x400/demo/Component.js
+++ src/com/0x400/demo/Component.js
@@ -4,19 +4,15 @@ sap.ui.define([
     "sap/ui/model/resource/ResourceModel"
 ], function (UIComponent, JSONModel, ResourceModel) {
     return UIComponent.extend("com.0x400.demo.Component", {
+        metadata: {
+            manifest: "json"
+        },
         init: function () {
             UIComponent.prototype.init.apply(this);
             var oModel = new JSONModel({
                 recipient: { firstName: "Pylon", lastName: "Syncher"}
             });
             this.setModel(oModel);
-
-            var i18nModel = new ResourceModel({
-                bundleName: "com.0x400.demo.i18n.i18n",
-                supportedLocales: [""],
-                fallbackLocale: ""
-            });
-            this.setModel(i18nModel, "i18n");
         }
     });
 });
