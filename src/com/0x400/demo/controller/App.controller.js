fa4fbaf20c3402d91a54416d1608912a68890670
Syncher Pylon, Peng <syncviip@gmail.com>
2020-08-27 14:46:31 +0800

    feat: step 7 - JSON Model

--- src/com/0x400/demo/controller/App.controller.js
+++ src/com/0x400/demo/controller/App.controller.js
@@ -1,8 +1,15 @@
 sap.ui.define([
     "sap/ui/core/mvc/Controller",
-    "sap/m/MessageToast"
-], function (Controller, MessageToast) {
+    "sap/m/MessageToast",
+    "sap/ui/model/json/JSONModel"
+], function (Controller, MessageToast, JSONModel) {
     return Controller.extend("com.0x400.demo.controller.App", {
+        onInit: function() {
+            var oModel = new JSONModel({
+                recipient: { firstName: "Pylon", lastName: "Syncher"}
+            });
+            this.getView().setModel(oModel);
+        },
         onSayHello: function () {
             MessageToast.show("Hello");
         }
